a3f42c5539eafb9c9166bd491c36151e2a43482c
zkan <kan@odds.team>
2026-03-31 07:43:54 +0700

    Move CatDto to dto folder under cats

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { CreateCatDto } from '../create-cat.dto';
+import { CreateCatDto } from './dto';
 
 @Controller('cats')
 export class CatsController {
